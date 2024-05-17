1be0ceb972c119b506deb4d8ff1793ec94636532
drew york <119648808+dre-yo@users.noreply.github.com>
2024-05-17 00:18:36 -0700

    fixed home page

--- src/app/components/welcome.tsx
+++ src/app/components/welcome.tsx
@@ -1,10 +1,12 @@
+
 export default function WelcomeScreen({ onStart }: { onStart: () => void }) {
     return (
-        <div className="flex flex-col items-center justify-center text-center">
-            <h1 className="text-4xl font-bold mb-4">Welcome</h1>
-            <p className="mb-4">Are you ready to start the quiz?</p>
+        <div className="flex flex-col items-center justify-center h-screen w-screen text-center text-black bg-[url('../../public/image/welcome.png')] bg-no-repeat bg-center">
+            <h1 className="text-4xl font-bold">What is your</h1>
+            <h1 className="text-4xl font-bold italic mb-4">Super power?</h1>
+            <p className="mb-4 font-medium">Learn what powers you possess</p>
             <button
-                className="btn btn-active btn-neutral"
+                className="btn btn-outline border-2 rounded-3xl w-40 text-lg"
                 onClick={onStart}
             >
                 Start
