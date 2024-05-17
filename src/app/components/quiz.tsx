0fb0ae0fe7d333b863934f2b24afffb60eae3947
drew york <119648808+dre-yo@users.noreply.github.com>
2024-05-17 00:59:32 -0700

    changes to button press animation

--- src/app/components/quiz.tsx
+++ src/app/components/quiz.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from "react";
+import React, { useState } from "react";
 import { questions } from "../data/questions";
 import Envelope from '../../../public/image/envelope.svg'
 
@@ -22,20 +22,28 @@ export default function QuestionComponent({ onNextQuestion }: { onNextQuestion: 
         setSelected(optionKey);
         const selectedValue = questions[index].options[optionKey]; // Accessing the value using the key
         setSelectedOption(selectedValue);
-        next(selectedValue);
+
+        const button = document.getElementById(optionKey);
+        if (button) {
+            button.classList.add('selected'); // Add animation class
+            setTimeout(() => {
+                button.classList.remove('selected');
+                next(selectedValue);
+            }, 500); // Animation duration
+        }
     };
 
     return (
         <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden text-black rounded-2xl">
-            <img src="" alt="" />
+            <img src={Envelope} alt="Envelope" />
             <h1 className="mt-6 text-2xl text-center">Question {questions[index].id}</h1>
             <h2 className="mt-4 text-center w-[18rem]">{questions[index].text}</h2>
             <ul className="flex flex-col items-center justify-center text-center">
                 {Object.entries(questions[index].options).map(([key, value]) => (
                     <button
                         key={key}
-                        className={`btn btn-outline border-2 rounded-3xl mt-3 h-[5rem] w-[17rem] font-normal cursor-pointer flex items-center justify-center ${selected === key ? "bg-gray-600 text-white" : "bg-white text-black"
-                            }`}
+                        id={key}
+                        className={`btn btn-outline border-2 rounded-3xl mt-3 h-[5rem] w-[17rem] font-normal cursor-pointer flex items-center justify-center ${selected === key ? 'bg-[#98e888] text-white' : 'text-black'}`}
                         onClick={() => handleOptionSelect(key)}
                     >
                         {key}
