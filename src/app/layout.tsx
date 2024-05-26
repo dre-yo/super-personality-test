eb05a1d064804ed78cd52a84d9c09770cc823da6
drew york <119648808+dre-yo@users.noreply.github.com>
2024-05-25 22:27:37 -0700

    another method

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { GoogleTagManager } from '@next/third-parties/google'
 import "./globals.css";
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: "Super Personality Test",
@@ -33,8 +34,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${helvetica.variable} font-sans`}>
+      <Script
+        id="gtm"
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={
+          {
+            __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KKM3DF49');`
+          }
+        }></Script>
       <body>{children}</body>
-      <GoogleTagManager gtmId="G-J2F6NT510N" />
     </html>
   );
 }
